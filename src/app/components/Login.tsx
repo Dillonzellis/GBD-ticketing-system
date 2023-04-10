50e8e75323289e296068f734c25dd0c23872b094
Dillon Ellis <dillonzellis@gmail.com>
2023-04-09 23:29:45 -0400

    login component text

--- src/app/components/Login.tsx
+++ src/app/components/Login.tsx
@@ -4,10 +4,10 @@ import { signIn } from "next-auth/react";
 
 function Login() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center text-center">
+    <div className="flex h-screen flex-col items-center justify-center text-center text-white">
       <div className="space-y-8">
         <h1 className="text-3xl">Login Screen</h1>
-        <button onClick={() => signIn("google")}>Sign in big dawgie</button> 
+        <button onClick={() => signIn("google")}>Sign in big dawgie</button>
       </div>
     </div>
   );
