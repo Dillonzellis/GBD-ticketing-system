3ca6428ce5e1e255c05514edc40b19c7b91e1941
Dillon Ellis <dillonzellis@gmail.com>
2023-04-16 15:35:54 -0400

    toggle add client form

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { useSession } from "next-auth/react";
+import { useState } from "react";
 import { AddClient } from "./AddClient";
 import NewTask from "./NewTask";
 
 const AdminPage = () => {
   const { data: session } = useSession();
+
+  const [showAddClient, setShowAddClient] = useState(false);
+
+  const handleAddClientClick = () => {
+    setShowAddClient(!showAddClient);
+  };
+
   return (
     <div className="container mx-auto pt-12 text-white">
       <div className="pb-10 text-3xl">Admin Page</div>
@@ -15,7 +23,10 @@ const AdminPage = () => {
       {/* Add Task Component */}
       {/* <NewTask /> */}
       {/* Add Client Component */}
-      <AddClient />
+      <button onClick={handleAddClientClick}>
+        {showAddClient ? "Hide Form" : "Add Client"}
+      </button>
+      {showAddClient && <AddClient />}
     </div>
   );
 };
